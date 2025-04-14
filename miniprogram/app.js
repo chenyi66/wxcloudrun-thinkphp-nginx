637fb0441d51fdca46e3f68a6d7516ed1b30d9d2
陈益 <chenyi@MacBook-Pro-5.local>
2025-04-14 09:48:51 +0800

    提交88

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 App({
     globalData: {
         // baseUrl: 'http://localhost:27081',
-        baseUrl: 'https://thinkphp-nginx-5cpb-151538-4-1352299247.sh.run.tcloudbase.com:27081',
+        baseUrl: 'https://thinkphp-nginx-5cpb-151538-4-1352299247.sh.run.tcloudbase.com',
         userInfo: null
     },
 
